3da84c8f495ccb2b8ed10bf414c4aea62b0e35e2
Alfredo Clark <alfredoclarkcardenas@gmail.com>
2022-04-20 14:19:34 -0700

    Dropdown game menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -13,16 +13,49 @@
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
             <a class="nav-link" href="#about"
               ><router-link to="/">Home</router-link></a
             >
           </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Games
+            </a>
+            <ul
+              class="dropdown-menu dropdown-menu-dark"
+              aria-labelledby="navbarDropdown"
+            >
+              <li>
+                <a class="dropdown-item" href="#"
+                  ><router-link to="/RPS">Rock Paper Scissors</router-link></a
+                >
+              </li>
+              <li>
+                <a class="dropdown-item" href="#"
+                  ><router-link to="/about">Tetris</router-link></a
+                >
+              </li>
+              <li>
+                <a class="dropdown-item" href="#"
+                  ><router-link to="/about">Rock Paper Scissors</router-link></a
+                >
+              </li>
+            </ul>
+          </li>
+
           <li class="nav-item">
-            <a class="nav-link" href="#skills"
-              ><router-link to="/about">About</router-link></a
+            <a class="nav-link" href="https://www.alfclark.dev/"
+              >Back to Portfolio</a
             >
           </li>
         </ul>
@@ -42,8 +75,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 nav {
-  padding: 30px;
-  background-color: rgb(37, 36, 36);
+  background-color: var(--dark);
 }
 
 nav a {
@@ -53,7 +85,7 @@ nav a {
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: var(--green);
   text-decoration: underline;
 }
 </style>
