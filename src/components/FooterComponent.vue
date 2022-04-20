c2bfb193ad52babd458dff66dc80a3215013daa8
Alfredo Clark <alfredoclarkcardenas@gmail.com>
2022-04-20 14:19:48 -0700

    Footer component done

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="footer">
+    <p>Alfredo Clark © 2022</p>
+  </footer>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: var(--dark);
+  color: var(--green);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem 0;
+}
+footer p {
+  margin: 0;
+}
+</style>
