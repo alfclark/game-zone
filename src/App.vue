94bb46eeee9d863958f01d1107e3edf5c90286ab
Alfredo Clark <alfredoclarkcardenas@gmail.com>
2022-04-20 14:18:24 -0700

    Screen size set, anymations and variables

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,62 @@
 <template>
   <NavBar />
+  <FooterComponent />
 </template>
 
 <script>
 import NavBar from "./components/NavBar.vue";
+import FooterComponent from "./components/FooterComponent.vue";
+
 export default {
   name: "App",
   components: {
     NavBar,
+    FooterComponent,
   },
 };
 </script>
 
 <style>
+:root {
+  --green: #42b983;
+  --dark: rgb(37, 36, 36);
+}
 @keyframes shift1 {
   0% {
-    transform: translateX(0);
+    transform: translateY(0);
   }
   50% {
-    transform: translateX(-20px);
+    transform: translateY(-20px);
   }
   100% {
-    transform: translateX(0);
+    transform: translateY(0);
+  }
+}
+@keyframes heartbeat {
+  0% {
+    animation-timing-function: ease-out;
+    transform: scale(1);
+    transform-origin: center center;
+  }
+
+  10% {
+    animation-timing-function: ease-in;
+    transform: scale(0.91);
+  }
+
+  17% {
+    animation-timing-function: ease-out;
+    transform: scale(0.98);
+  }
+
+  33% {
+    animation-timing-function: ease-in;
+    transform: scale(0.87);
+  }
+
+  45% {
+    animation-timing-function: ease-out;
+    transform: scale(1);
   }
 }
 * {
@@ -36,5 +71,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   background-color: rgb(58, 58, 58);
+  height: 100vh;
 }
 </style>
