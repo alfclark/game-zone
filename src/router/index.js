70ae0e07e255cb9e35aae99959290c44ec18cbf2
Alfredo Clark <alfredoclarkcardenas@gmail.com>
2022-04-20 14:19:24 -0700

    Path fix

--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,9 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/RockPaperScissors.vue"),
+    path: "/RPS",
+    name: "RPS",
+    component: () => import("../views/RockPaperScissors.vue"),
   },
 ];
 
