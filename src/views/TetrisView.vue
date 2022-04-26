b3e4c7f547777e3add0d5ac0f35e7fff3b8c2be4
Alfredo Clark <alfredoclarkcardenas@gmail.com>
2022-04-25 19:37:41 -0700

    Coming Soon

--- src/views/TetrisView.vue
+++ src/views/TetrisView.vue
@@ -1,13 +1,16 @@
 <template>
   <Game />
+  <ComingSoon />
 </template>
 
 <script>
 import Game from "@/components/tetris/GameComponent.vue";
+import ComingSoon from "@/components/ComingSoon.vue";
 
 export default {
   components: {
     Game,
+    ComingSoon,
   },
 };
 </script>
--- src/views/TicTacToe.vue
+++ src/views/TicTacToe.vue
@@ -1,13 +1,16 @@
 <template>
   <Game />
+  <ComingSoon />
 </template>
 
 <script>
 import Game from "@/components/tic-tac-toe/GameComponent.vue";
+import ComingSoon from "@/components/ComingSoon.vue";
 
 export default {
   components: {
     Game,
+    ComingSoon,
   },
 };
 </script>
