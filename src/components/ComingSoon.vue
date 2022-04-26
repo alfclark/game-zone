06a2c99706a872fd0df266bc2e5f5696e4aad6a4
Alfredo Clark <alfredoclarkcardenas@gmail.com>
2022-04-25 19:36:31 -0700

    Coming Soon component created

---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2 class="msg">Coming Soon!</h2>
+  </div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.msg {
+  font-size: 3rem;
+  margin-top: 10rem;
+  color: white;
+  background-color: var(--green);
+}
+</style>
